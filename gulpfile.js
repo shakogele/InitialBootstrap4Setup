c8d536d2341b0dde67148b451bffded3246ce6c6
shakogele <shakogele@gmail.com>
2018-08-26 23:58:04 +0400

    Importing Bootstrap.scss

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('js', function() {
-  return gulp.src(['node_modules/bootstrap/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+  return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
              .pipe(gulp.dest('src/js'))
              .pipe(browsersync.stream());
 });
