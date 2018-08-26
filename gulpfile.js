75173996e4f265e5d91c1eecc10b48033c2487fa
shakogele <shakogele@gmail.com>
2018-08-26 22:59:17 +0400

    Gulp File

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,19 @@ var gulp = require('gulp');
 var browsersync = require('browser-sync');
 var sass = require('gulp-sass');
 
+gulp.task('sass', function() {
+  return gulp.src('node_modules/bootstrap/scss/bootstrap.scss', 'src/scss/*.scss')
+             .pipe(sass())
+             .pipe(gulp.dest('src/css'))
+             .pipe(browsersync.stream());
+});
+
+gulp.task('js', function() {
+  return gulp.src(['node_modules/bootstrap/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+             .pipe(gulp.dest('src/js'))
+             .pipe(browsersync.stream());
+});
+
 gulp.task('serve', ['sass'], function() {
 
   browsersync.init({
